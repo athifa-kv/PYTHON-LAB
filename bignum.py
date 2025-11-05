10c1ca57b5ea95d3adbfc103aa5a0825e39f8937
athifa-kv <kvathifa@gmail.com>
2025-11-05 14:38:02 +0530

    Add files via upload

---
+++ bignum.py
@@ -0,0 +1,12 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+if a>b and a>c:
+    print("The biggest number is:",a)
+elif b>a and b>c:
+    print("The biggest number is:",b)
+else:  
+    print("The biggest number is:",c)
+
+
+
